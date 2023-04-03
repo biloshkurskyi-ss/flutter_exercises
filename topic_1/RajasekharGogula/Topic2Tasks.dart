1d30c5f6a4d7a799f0af6e7d3632a05b71795326
Gogula Rajasekhar <iosrajasekhar@gmail.com>
2023-04-03 14:56:09 +0530

    topic tasks done.

--- topic_1/RajasekharGogula/Topic2Tasks.dart
+++ topic_1/RajasekharGogula/Topic2Tasks.dart
@@ -12,6 +12,12 @@ void main(List<String> args) {
   //3:
   print(getGreaterNumber(40, 30)); 
 
+  //4:
+  print(getLetterGradeSwitch(90));
+  print(getLetterGradeSwitch(50));
+
+
+
 }
 //1: Logic 1
 void compareTheTwoNumbers(int a , int b) {
@@ -59,4 +65,59 @@ String getLetterGrade(double grade) {
 //3: logic
 int getGreaterNumber(int a, int b) {
   return a > b ? a : b;
+}
+
+//4 : logic 
+
+String getLetterGradeSwitch(double grade) {
+  int roundedGrade = grade.round();
+  switch (roundedGrade) {
+    case 90:
+    case 91:
+    case 92:
+    case 93:
+    case 94:
+    case 95:
+    case 96:
+    case 97:
+    case 98:
+    case 99:
+    case 100:
+      return 'A';
+    case 80:
+    case 81:
+    case 82:
+    case 83:
+    case 84:
+    case 85:
+    case 86:
+    case 87:
+    case 88:
+    case 89:
+      return 'B';
+    case 70:
+    case 71:
+    case 72:
+    case 73:
+    case 74:
+    case 75:
+    case 76:
+    case 77:
+    case 78:
+    case 79:
+      return 'C';
+    case 60:
+    case 61:
+    case 62:
+    case 63:
+    case 64:
+    case 65:
+    case 66:
+    case 67:
+    case 68:
+    case 69:
+      return 'D';
+    default:
+      return 'F';
+  }
 }
