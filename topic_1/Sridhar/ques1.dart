4d5a7a00d7ed5b3f2a8fbbf4d706b3baeeb50a76
Sridhar <sridharonstar@gmail.com>
2023-04-03 18:27:52 +0530

    Topic_1 Solution

---
+++ topic_1/Sridhar/ques1.dart
@@ -0,0 +1,8 @@
+void main() {
+  var list = [1, 2, 3, 4];
+  var sum = 0;
+  for (var i = 0; i < list.length; i++) {
+    sum += list[i];
+  }
+  print(sum);
+}
---
+++ topic_1/Sridhar/ques2.dart
@@ -0,0 +1,8 @@
+void main() {
+  double pi = 3.14159;
+  int r = 6;
+  double area = pi*r*r;
+  print(area);
+ 
+
+} 
